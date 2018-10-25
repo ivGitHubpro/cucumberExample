6b4e694d5c5b152c48f6ba2d44883b363899257a
Ivanka Stoilova <ivastoilova24@gmail.com>
2018-10-25 16:40:06 +0300

    Now I have experience

--- Desktop/QA Complete/Selenium/Homework/Homework-My-Solutions/GitLearning/src/com/company/Main.java
+++ Desktop/QA Complete/Selenium/Homework/Homework-My-Solutions/GitLearning/src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is my first examples with git");
+        System.out.println("I have little experience yet");
     }
 }
